260ad2a014122bbf57fdf585594d059f67dabfdd
Nosang Subba <nosangdev@gmail.com>
2024-07-19 16:30:43 +0530

    changed favicon

--- app/script.ts
+++
@@ -1,9 +0,0 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   document.addEventListener("mousemove", (e) => {
-//     const cursor = document.getElementById("cursor");
-//     if (cursor) {
-//       cursor.style.left = `${e.clientX}px`;
-//       cursor.style.top = `${e.clientY}px`;
-//     }
-//   });
-// });
