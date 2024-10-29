4b914b888ef7923951c087fb2cf2365e009dd912
Nosang Subba <nosangdev@gmail.com>
2024-10-29 21:36:01 +0530

    update skills

--- data.ts
+++ data.ts
@@ -7,6 +7,6 @@ export const experiences: Array<Experience> = [
     company: "Fi Ellements",
     description:
       "As an Associate Software Engineer at Fi Ellements from February 2022 to August 2023, I delivered technical support across multiple projects, ensuring high levels of user satisfaction. I collaborated closely with design teams to implement web designs from Figma into intuitive, user-friendly interfaces. I enhanced software functionality through API integrations and actively participated in cross-functional meetings to foster team collaboration. Additionally, I mentored interns, guiding their professional development and integration into the team.",
-    skills: ["HTML", "CSS", "ReactJs", "JavaScript", "NodeJs"],
+    skills: ["HTML", "CSS", "React", "JavaScript", "Node.js"],
   },
 ];
